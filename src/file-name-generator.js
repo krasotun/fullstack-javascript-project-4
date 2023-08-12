0f0cbadbce1d0931bdf5e9adb476de38bc575957
krasotun <servrostov@gmail.com>
2023-08-12 11:17:47 +0300

    add test

--- src/file-name-generator.js
+++ src/file-name-generator.js
@@ -1,3 +1,14 @@
-const regEx = /[^0-9a-zA-Z]/;
+const regEx = /[^0-9a-zA-Z]/g;
 
-export default (url) => url;
+export default (url) => {
+  let newString = '';
+  if (url.startsWith('https://')) {
+    newString = url.replace('https://', '');
+  }
+
+  if (url.startsWith('http://')) {
+    newString = url.replace('http://', '');
+  }
+
+  return `${newString.trim().replace(regEx, '-')}.html`;
+};
