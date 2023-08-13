ba32224d76f15ecfe6d4475263f054b02ef25392
krasotun <servrostov@gmail.com>
2023-08-13 12:02:21 +0300

    add test for page-loader

---
+++ __tests__/page-loader.spec.js
@@ -0,0 +1,46 @@
+/* eslint-disable implicit-arrow-linebreak */
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs/promises';
+import os from 'os';
+import nock from 'nock';
+import { beforeEach, describe, expect } from '@jest/globals';
+import pageLoader from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) =>
+  path.join(__dirname, '..', '__fixtures__', filename);
+
+const readFile = (filename) => fs.readFile(getFixturePath(filename), 'utf-8');
+
+describe('#pageLoader', () => {
+  let incomingUrl;
+  let expectedData;
+  let expectedFileName;
+  let tempDirPath;
+
+  beforeEach(async () => {
+    incomingUrl = await readFile('incoming-url.txt');
+    expectedData = await readFile('expected-data.txt');
+    expectedFileName = await readFile('expected-file-name.txt');
+    nock(incomingUrl).get('').reply(200, expectedData);
+    tempDirPath = await fs.mkdtemp(path.join(os.tmpdir(), 'page-loader-'));
+  });
+
+  afterEach(async () => {
+    if (tempDirPath) {
+      await fs.rmdir(tempDirPath, { recursive: true });
+    }
+  });
+
+  it('should save data to html file', async () => {
+    await pageLoader(tempDirPath, incomingUrl);
+    const savedData = await fs.readFile(
+      `${tempDirPath}/${expectedFileName}`,
+      'utf-8',
+    );
+    expect(savedData).toBe(expectedData);
+  });
+});
